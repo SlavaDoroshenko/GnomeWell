00c1d139b1753433adaafc4c170fa14bd90119dc
Slava Doroshenko <lblue1256@gmail.com>
2020-02-13 17:08:28 +0400

    Physic for Rope, Interface and Camera

---
+++ Assets/Scripts/Swinging.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swinging : MonoBehaviour
+{
+
+    public float swingSensivity = 100.0f;
+    
+    void FixedUpdate()
+    {
+        if (GetComponent<Rigidbody2D>() == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        float swing = InputManager.instance.sidewaysMotion;
+
+        Vector2 force = new Vector2(swing * swingSensivity, 0);
+
+        GetComponent<Rigidbody2D>().AddForce(force);
+    }
+}
