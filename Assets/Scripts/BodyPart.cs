a9a946a25a437958b347eb79c4f6eaa7f3c2fec1
unknown <lblue1256@gmail.com>
2020-02-14 20:05:32 +0400

    BodyPart and Gnome scripts

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BodyPart : MonoBehaviour
+{
+    public Sprite detachedSprite;
+    public Sprite burnedSprite;
+
+    public Transform bloodFontainOrigin;
+
+    bool detached = false;
+
+    public void Detached()
+    {
+        detached = true;
+
+        this.tag = "Untagged";
+
+        transform.SetParent(null, true);
+    }
+
+    private void Update()
+    {
+        if (!detached)
+        {
+            return;
+        }
+
+        var rigidbody = GetComponent<Rigidbody2D>();
+
+        if (rigidbody.IsSleeping())
+        {
+            foreach (Joint2D joint in GetComponents<Joint2D>())
+            {
+                Destroy(joint);
+            }
+
+            foreach (Rigidbody2D rigidbody1 in GetComponents<Rigidbody2D>())
+            {
+                Destroy(rigidbody1);
+            }
+
+            foreach (Collider2D collider in GetComponents<Collider2D>())
+            {
+                Destroy(collider);
+            }
+
+            Destroy(this);
+        }
+    }
+
+    public void ApplyDamageSprite(Gnome.DamageType damageType)
+    {
+        Sprite spriteToUse = null;
+
+        switch (damageType) {
+            case Gnome.DamageType.Burning: spriteToUse = burnedSprite;
+            break;
+            case Gnome.DamageType.Slicing: spriteToUse = detachedSprite;
+            break;
+        }
+
+        if (spriteToUse != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = spriteToUse;
+        }
+    }
+}
---
+++ Assets/Scripts/Gnome.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gnome : MonoBehaviour
+{
+    public Transform cameraFollowTarget;
+
+    public Rigidbody2D ropeBody;
+
+    public Sprite armHoldingEmpty;
+    public Sprite armHoldingTreasure;
+
+    public SpriteRenderer holdingArm;
+
+    public GameObject deathPrefab;
+    public GameObject flameDeatPrefab;
+    public GameObject ghostPrefab;
+
+    public float delayBeforeRemoving = 3.0f;
+    public float delayBeforeReleasingGost = 0.25f;
+
+    public GameObject bloodFontainPrefab;
+
+    bool dead = false;
+
+    bool _holdingTreasure = false;
+
+    public bool holdingTreasure
+    {
+        get
+        {
+            return _holdingTreasure;
+        }
+
+        set
+        {
+            if (dead == true)
+            {
+                return;
+            }
+
+            _holdingTreasure = value;
+
+            if (holdingArm != null)
+            {
+                if (_holdingTreasure)
+                {
+                    holdingArm.sprite = armHoldingTreasure;
+                }
+                else
+                {
+                    holdingArm.sprite = armHoldingEmpty;
+                }
+            }
+        }
+    }
+
+    public enum DamageType
+    {
+        Slicing,
+        Burning
+    }
+
+    public void ShowDamageEffect(DamageType type)
+    {
+        switch (type)
+        {
+            case DamageType.Burning:
+                if (flameDeatPrefab != null)
+                {
+                    Instantiate(flameDeatPrefab, cameraFollowTarget.position, cameraFollowTarget.rotation);
+                }
+                break;
+
+            case DamageType.Slicing:
+                if (deathPrefab != null)
+                {
+                    Instantiate(deathPrefab, cameraFollowTarget.position, cameraFollowTarget.rotation);
+                }
+                break;
+        }
+    }
+
+    public void DestroyGnome(DamageType type)
+    {
+        holdingTreasure = false;
+
+        dead = true;
+
+        foreach (BodyPart part in GetComponentsInChildren<BodyPart>())
+        {
+            switch (type)
+            {
+                case DamageType.Burning:
+                    bool shouldBurn = Random.Range(0, 2) == 0;
+                    if (shouldBurn)
+                    {
+                        part.ApplyDamageSprite(type);
+                    }
+                    break;
+
+                case DamageType.Slicing:
+                    part.ApplyDamageSprite(type);
+                    break;
+            }
+
+            bool shouldDetach = Random.Range(0, 2) == 0;
+
+            if (shouldDetach)
+            {
+                part.Detached();
+            }
+
+            if (part.bloodFontainOrigin != null && bloodFontainPrefab != null)
+            {
+                GameObject fountain = Instantiate(bloodFontainPrefab, part.bloodFontainOrigin.position, part.bloodFontainOrigin.rotation) as GameObject;
+
+                fountain.transform.SetParent(this.cameraFollowTarget, false);
+            }
+
+            var allJoints = part.GetComponentsInChildren<Joint2D>();
+            foreach (Joint2D joint in allJoints)
+            {
+                Destroy(joint);
+            }
+        }
+
+        var remove = gameObject.AddComponent<RemoveAfterDalay>();
+        remove.delay = delayBeforeRemoving;
+
+        StartCoroutine(ReleaseGhost());
+    }
+
+    IEnumerator ReleaseGhost()
+    {
+        if (ghostPrefab == null)
+        {
+            yield break;
+        }
+
+        yield return new WaitForSeconds(delayBeforeReleasingGost);
+
+        Instantiate(ghostPrefab, transform.position, Quaternion.identity);
+    }
+}
